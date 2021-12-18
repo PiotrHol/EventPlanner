9c9ab1f87e2fa2037932c9eb92f61e809266ac04
PiotrHol <piotrekholub@wp.pl>
2021-12-18 16:24:33 +0100

    Add login support in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
-import "./scss/_app.scss"
+import React, {useEffect, useState} from "react";
+import "./scss/app.scss";
+import appBase from "./firebase";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {LogBox} from "./LogBox";
+import {Home} from "./Home";
 
 const App = () => {
-  return <div></div>;
+    const [isUser, setIsUser] = useState(false);
+    const [userId, setUserId] = useState("");
+
+    useEffect(() => {
+        onAuthStateChanged(getAuth(), (user) => {
+            if (user) {
+                setUserId(user.uid);
+                setIsUser(true);
+            } else {
+                setUserId("");
+                setIsUser(false);
+            }
+        });
+    }, [userId]);
+
+    return (
+        <div className="appContainer">
+            {!isUser ? <LogBox setUser={(user) => setUserId(user)}/> : <Home user={userId} removeUser={() => setUserId("")}/>}
+        </div>
+    );
 }
 
 export default App;
