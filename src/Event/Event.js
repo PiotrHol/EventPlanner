604628353860196e3656a4586a1472b7007cdb76
PiotrHol <piotrekholub@wp.pl>
2022-01-05 23:39:50 +0100

    Add Tasks component to Event component

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./event.scss";
 import { Information } from "../Information/Information";
+import { Tasks } from "../Tasks/Tasks";
 
 export const Event = ({userId, id, name, place, date, tasks, quests, eventUpdate }) => {
   return (
     <div className="eventPage">
       <div className="eventPage--leftContent">
         <Information user={userId} eventId={id} name={name} place={place} date={date} updateEvent={eventUpdate} />
+        <Tasks user={userId} eventId={id} tasks={tasks} updateEvent={eventUpdate} />
       </div>
       <div className="eventPage--rightContent">
 
