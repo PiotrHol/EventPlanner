71ed09fff2ba6d0a4d1d37f2c39c1a4508341078
PiotrHol <piotrekholub@wp.pl>
2022-01-05 12:00:50 +0100

    Add Information component to Event component

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./event.scss";
-import { Link } from "react-router-dom";
+import { Information } from "../Information/Information";
 
 export const Event = ({ id, name, place, date, tasks, quests, eventUpdate }) => {
   return (
     <div className="eventPage">
-        <Link to="/home/events">{name}</Link>
+      <div className="eventPage--leftContent">
+        <Information id={id} name={name} place={place} date={date} />
+      </div>
+      <div className="eventPage--rightContent">
+
+      </div>
     </div>
   );
 };
