0292631af375804e0d470aa0001382ccdb36b99e
PiotrHol <piotrekholub@wp.pl>
2022-01-10 18:19:56 +0100

    Add button handler in Guest component

--- src/Guest/Guest.js
+++ src/Guest/Guest.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./guest.scss";
+import { doc, updateDoc, arrayRemove, getFirestore } from "firebase/firestore";
 
 export const Guest = ({userId, eventId, guest, eventUpdate}) => {
+    const deleteGuestBtnHandler = () => {
+        updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
+            guests: arrayRemove(guest)
+        }).then(() => {
+            eventUpdate(prev => prev.map(event => {
+                if (event.id === eventId) {
+                    event.guests = event.guests.filter(singleGuest => singleGuest.id !== guest.id);
+                }
+                return event;
+            }));
+        });
+    }
+
     return (
         <div className="eventPage--guest">
             <h3>{guest.name}</h3>
-            <span className="fas fa-trash" />
+            <span className="fas fa-trash" title="Usuń" onClick={deleteGuestBtnHandler} />
         </div>
     );
 }
