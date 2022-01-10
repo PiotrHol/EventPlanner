9cfeb2137f5fbf3c30707fa8fe09cb178df52057
PiotrHol <piotrekholub@wp.pl>
2022-01-10 17:00:33 +0100

    Add button handler in Guests component

--- src/Guests/Guests.js
+++ src/Guests/Guests.js
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
 import "./guests.scss";
+import { eventValidation } from "../validation";
+import { doc, updateDoc, arrayUnion, getFirestore } from "firebase/firestore";
 
 export const Guests = ({user, eventId, guests, updateEvent}) => {
     const [isAddGuest, setIsAddGuest] = useState(false);
-    const [name, setName] = useState("");
+    const [guestName, setGuestName] = useState("");
     const [isValid, setIsValid] = useState(true);
 
     const addGuestBtnHandler = () => {
         setIsAddGuest(prev => !prev);
-        setName("");
+        setGuestName("");
+        !isValid && setIsValid(true);
+    }
+
+    const saveGuestBtnHandler = event => {
+        event.preventDefault();
+
+        if (!eventValidation(guestName)) {
+            setIsValid(false);
+            return;
+        }
+
+        const dataToSet = {
+            id: Date.now(),
+            name: guestName
+        }
+
+        updateDoc(doc(getFirestore(), "users", user, "events", eventId), {
+            guests: arrayUnion(dataToSet)
+        }).then(() => {
+            updateEvent(prev => prev.map(event => {
+                if (event.id === eventId) {
+                    event.guests.push(dataToSet);
+                }
+                return event;
+            }));
+        });
+
+        setIsAddGuest(false);
+        setGuestName("");
+        !isValid && setIsValid(true);
     }
 
     return (
@@ -21,16 +53,16 @@ export const Guests = ({user, eventId, guests, updateEvent}) => {
                         <div className="eventPage--guestes__newInputs">
                             <label className="eventPage--guests__newTittle">
                                 <h3>Imię i nazwisko:</h3>
-                                <input type="text" maxLength={50} value={name} onChange={event => setName(event.target.value)} />
+                                <input type="text" maxLength={50} value={guestName} onChange={event => setGuestName(event.target.value)} />
                             </label>
-                            <button className="eventPage--guests__newGuestBtn">Dodaj</button>
+                            <button className="eventPage--guests__newGuestBtn" onClick={saveGuestBtnHandler}>Dodaj</button>
                         </div>
                         {!isValid && <p className="eventPage--guests__wrongInput">Pole nie może być puste!</p>}
                 </form>
                 )}
                 <div className="eventPage--guests__list">
                     <h3 className="eventPage--guests__listTittle">Lista gości ({guests.length} {guests.length > 4 || guests.length < 2 
-                    ? "osób" : "osoby"}):</h3>
+                    ? (guests.length === 1 ? "osoba" : "osób") : "osoby"}):</h3>
                 </div>
             </div>
         </div>
