c9cdf493cd1f0e2fb8326d92b9f5363158641dd5
PiotrHol <piotrekholub@wp.pl>
2022-01-20 12:00:15 +0100

    Changed functions with firebase methods to async functions in Task component

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -4,44 +4,43 @@ import { doc, updateDoc, arrayUnion, arrayRemove, getFirestore } from "firebase/
 import classNames from "classnames";
 
 export const Task = ({userId, eventId, task, eventUpdate}) => {
-    const doneBtnHandler = () => {
+    const doneBtnHandler = async () => {
         const dataToSet = {
             ...task,
             isDone: !task.isDone
         }
 
-        updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
+        await updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
             tasks: arrayRemove(task)
-        }).then(() => {
-            updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
-                tasks: arrayUnion(dataToSet)
-            }).then(() => {
-                eventUpdate(prev => prev.map(event => {
-                    if (event.id === eventId) {
-                        event.tasks = event.tasks.map(singleTask => {
-                            if (singleTask.id === task.id) {
-                                return dataToSet;
-                            }
-                            return singleTask;
-                        });
-                    }
-                    return event;
-                }));
-            });
         });
+
+        await updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
+            tasks: arrayUnion(dataToSet)
+        });
+
+        eventUpdate(prev => prev.map(event => {
+            if (event.id === eventId) {
+                event.tasks = event.tasks.map(singleTask => {
+                    if (singleTask.id === task.id) {
+                        return dataToSet;
+                    }
+                    return singleTask;
+                });
+            }
+            return event;
+        }));
     }
 
-    const trashBtnHandler = () => {
-        updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
+    const trashBtnHandler = async () => {
+        await updateDoc(doc(getFirestore(), "users", userId, "events", eventId), {
             tasks: arrayRemove(task)
-        }).then(() => {
-            eventUpdate(prev => {
-                return prev.map(event => {
-                    if (event.id === eventId) {
-                         event.tasks = event.tasks.filter(({id}) => id !== task.id);
-                    }
-                    return event;
-                });
+        });
+        eventUpdate(prev => {
+            return prev.map(event => {
+                if (event.id === eventId) {
+                    event.tasks = event.tasks.filter(({id}) => id !== task.id);
+                }
+                return event;
             });
         });
     }
