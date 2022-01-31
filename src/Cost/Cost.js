f3afd9a65fc4f42d325bf662f9b8506ef06b3b46
Piotr Hołub <piotrekholub@wp.pl>
2022-01-31 02:32:38 +0100

    Changed class names in Cost component

--- src/Cost/Cost.js
+++ src/Cost/Cost.js
@@ -4,17 +4,17 @@ import "./cost.scss";
 export const Cost = ({tasks}) => {
 
     return (
-        <div className="eventPage__content eventPage__cost">
-            <div className="eventPage__bar"/>
-            <div className="eventPage__cost-content">
-                <div className="eventPage__cost-main">
+        <div className="event-page__content event-page__cost">
+            <div className="event-page__bar"/>
+            <div className="event-page__cost-content">
+                <div className="event-page__cost-main">
                     <h3>Koszty:</h3>
-                    <h3 className="eventPage__cost-main-value">{tasks.reduce((allCost, singleCost) => allCost + singleCost.cost, 0)} zł</h3>
+                    <h3 className="event-page__cost-main-value">{tasks.reduce((allCost, singleCost) => allCost + singleCost.cost, 0)} zł</h3>
                 </div>
-                <h3 className="eventPage__cost-single-title">Szczegółowe koszty:</h3>
-                <ul className="eventPage__cost-single">
+                <h3 className="event-page__cost-single-title">Szczegółowe koszty:</h3>
+                <ul className="event-page__cost-single">
                     {tasks.map(({id, name, cost}) => cost !== 0 && <li key={id}>
-                    <span className="fas fa-caret-right"/>{name} - <span className="eventPage__cost-single-value">{cost} zł</span>
+                    <span className="fas fa-caret-right"/>{name} - <span className="event-page__cost-single-value">{cost} zł</span>
                     </li>)}
                 </ul>
             </div>
