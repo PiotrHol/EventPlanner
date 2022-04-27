e05b8d9fb9017cc9be1f325485d0e3302604fa53
Piotr Hołub <piotrekholub@wp.pl>
2022-04-28 00:51:31 +0200

    Created and added redux store to project

---
+++ src/redux/reducers/eventsSlice.js
@@ -0,0 +1,13 @@
+const initialState = {
+  events: [],
+  archive: [],
+};
+
+const eventsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export { eventsReducer };
