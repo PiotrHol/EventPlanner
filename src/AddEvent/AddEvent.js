174d469a3954ebc3cd64d45eb242c682b731b66e
Piotr Hołub <piotrekholub@wp.pl>
2022-09-12 00:25:32 +0200

    Added style for AddEvent component

--- src/AddEvent/AddEvent.js
+++ src/AddEvent/AddEvent.js
@@ -1,43 +1,82 @@
 import React, { useState } from "react";
 import "./addEvent.scss";
+import { Button } from "../Button/Button";
 
-export const AddEvent = () => {
+export const AddEvent = ({ setIsShow }) => {
   const [newEventName, setNewEventName] = useState("");
   const [newEventPlace, setNewEventPlace] = useState("");
   const [newEventDate, setNewEventDate] = useState("");
 
   return (
     <div className="add-event">
+      <h2 className="add-event__title">Dodaj wydarzenie</h2>
       <form>
         <div className="add-event__input-box">
-          <label htmlFor="new-event-name">Nazwa</label>
           <input
             type="text"
+            className="add-event__input"
             maxLength={50}
             id="new-event-name"
             value={newEventName}
             onChange={(e) => setNewEventName(e.target.value)}
           />
+          <label
+            className={
+              newEventName
+                ? "add-event__label add-event__label--focus"
+                : "add-event__label"
+            }
+            htmlFor="new-event-name"
+          >
+            Nazwa
+          </label>
         </div>
         <div className="add-event__input-box">
-          <label htmlFor="new-event-place">Miejsce</label>
           <input
             type="text"
+            className="add-event__input"
             maxLength={50}
             id="new-event-place"
             value={newEventPlace}
             onChange={(e) => setNewEventPlace(e.target.value)}
           />
+          <label
+            className={
+              newEventPlace
+                ? "add-event__label add-event__label--focus"
+                : "add-event__label"
+            }
+            htmlFor="new-event-place"
+          >
+            Miejsce
+          </label>
         </div>
         <div className="add-event__input-box">
-          <label htmlFor="new-event-date">Data</label>
           <input
-            type="date"
+            type="text"
+            className="add-event__input"
             value={newEventDate}
+            onFocus={(e) => (e.target.type = "date")}
+            onBlur={(e) => (e.target.type = "text")}
             onChange={(e) => setNewEventDate(e.target.value)}
           />
+          <label
+            className={
+              newEventDate
+                ? "add-event__label add-event__label--focus"
+                : "add-event__label"
+            }
+            htmlFor="new-event-date"
+          >
+            Data
+          </label>
         </div>
-        <button>Dodaj</button>
+        <Button
+          onClickFunction={() => setIsShow(false)}
+          icon="far fa-plus-square"
+          text="Zapisz"
+          isStatic={true}
+        />
       </form>
     </div>
   );
