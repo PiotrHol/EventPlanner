2893f2ff3973e2504d1a1306acc16a5e464ada42
Piotr Hołub <piotrekholub@wp.pl>
2022-09-11 20:11:45 +0200

    Added static version of Button component

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./button.scss";
 import { useState, useEffect, useRef } from "react";
 
-export const Button = ({ onClickFunction, icon, text }) => {
+export const Button = ({ onClickFunction, icon, text, isStatic }) => {
   const [firstButtonWidth, setFirstButtonWidth] = useState(0);
   const [buttonWidth, setButtonWidth] = useState("fit-content");
   const [afterRenderButtonWidth, setAfterRenderButtonWidth] = useState(0);
@@ -12,61 +12,69 @@ export const Button = ({ onClickFunction, icon, text }) => {
   const buttonRef = useRef();
 
   useEffect(() => {
-    setFirstButtonWidth(buttonRef.current.offsetWidth);
-    setButtonWidth(buttonRef.current.offsetHeight);
-    setAfterRenderButtonWidth(buttonRef.current.offsetHeight);
-    setWindowXDimension(window.innerWidth);
+    if (!isStatic) {
+      setFirstButtonWidth(buttonRef.current.offsetWidth);
+      setButtonWidth(buttonRef.current.offsetHeight);
+      setAfterRenderButtonWidth(buttonRef.current.offsetHeight);
+      setWindowXDimension(window.innerWidth);
 
-    return () => {
-      clearInterval(onHoverIntervalId);
-      clearInterval(offHoverIntervalId);
-    };
+      return () => {
+        clearInterval(onHoverIntervalId);
+        clearInterval(offHoverIntervalId);
+      };
+    }
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    const handleResize = () => {
-      setButtonWidth("fit-content");
-      setFirstButtonWidth(buttonRef.current.offsetWidth);
-      setButtonWidth(buttonRef.current.offsetHeight);
-      setAfterRenderButtonWidth(buttonRef.current.offsetHeight);
-    };
+    if (!isStatic) {
+      const handleResize = () => {
+        setButtonWidth("fit-content");
+        setFirstButtonWidth(buttonRef.current.offsetWidth);
+        setButtonWidth(buttonRef.current.offsetHeight);
+        setAfterRenderButtonWidth(buttonRef.current.offsetHeight);
+      };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
+      handleResize();
+      window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, [windowXDimension]);
 
   const onHoverFunction = () => {
-    if (window.innerWidth >= 768) {
-      if (buttonRef.current.offsetWidth < firstButtonWidth) {
-        clearInterval(offHoverIntervalId);
-      }
-      const intervalId = setInterval(() => {
+    if (!isStatic) {
+      if (window.innerWidth >= 768) {
         if (buttonRef.current.offsetWidth < firstButtonWidth) {
-          setButtonWidth((prev) => prev + 2);
-        } else {
-          clearInterval(intervalId);
+          clearInterval(offHoverIntervalId);
         }
-      }, 5);
-      setOnHoverIntervalId(intervalId);
+        const intervalId = setInterval(() => {
+          if (buttonRef.current.offsetWidth < firstButtonWidth) {
+            setButtonWidth((prev) => prev + 2);
+          } else {
+            clearInterval(intervalId);
+          }
+        }, 5);
+        setOnHoverIntervalId(intervalId);
+      }
     }
   };
 
   const offHoverFunction = () => {
-    if (window.innerWidth >= 768) {
-      if (buttonRef.current.offsetWidth > afterRenderButtonWidth) {
-        clearInterval(onHoverIntervalId);
-      }
-      const intervalId = setInterval(() => {
+    if (!isStatic) {
+      if (window.innerWidth >= 768) {
         if (buttonRef.current.offsetWidth > afterRenderButtonWidth) {
-          setButtonWidth((prev) => prev - 2);
-        } else {
-          clearInterval(intervalId);
+          clearInterval(onHoverIntervalId);
         }
-      }, 5);
-      setOffHoverIntervalId(intervalId);
+        const intervalId = setInterval(() => {
+          if (buttonRef.current.offsetWidth > afterRenderButtonWidth) {
+            setButtonWidth((prev) => prev - 2);
+          } else {
+            clearInterval(intervalId);
+          }
+        }, 5);
+        setOffHoverIntervalId(intervalId);
+      }
     }
   };
 
@@ -79,8 +87,20 @@ export const Button = ({ onClickFunction, icon, text }) => {
       onMouseLeave={offHoverFunction}
       onClick={onClickFunction}
     >
-      <span className={`${icon} button__icon`} />
-      <span className="button__text">{text}</span>
+      <span
+        className={
+          isStatic
+            ? `${icon} button__icon button__icon--static`
+            : `${icon} button__icon`
+        }
+      />
+      <span
+        className={
+          isStatic ? "button__text button__text--static" : "button__text"
+        }
+      >
+        {text}
+      </span>
     </div>
   );
 };
