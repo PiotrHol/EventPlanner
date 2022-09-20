34b2b6e13f926f5c0501d5c426b4ca848ef92602
Piotr Hołub <piotrekholub@wp.pl>
2022-09-20 23:41:28 +0200

    Added save button handler

--- src/AddEvent/AddEvent.js
+++ src/AddEvent/AddEvent.js
@@ -1,11 +1,61 @@
 import React, { useState } from "react";
 import "./addEvent.scss";
 import { Button } from "../Button/Button";
+import { setDoc, doc, getFirestore } from "firebase/firestore";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addEvent } from "../redux/actions/eventsActions";
 
 export const AddEvent = ({ setIsShow }) => {
   const [newEventName, setNewEventName] = useState("");
   const [newEventPlace, setNewEventPlace] = useState("");
   const [newEventDate, setNewEventDate] = useState("");
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  const addEventBtnHandler = async (event) => {
+    event.preventDefault();
+    // if (!eventValidation(eventName, eventPlace, eventDate)) {
+    //   setIsValidate(false);
+    //   return;
+    // }
+
+    const dataToSet = {
+      name: newEventName,
+      place: newEventPlace,
+      date: `${newEventDate.substr(8, 2)}.${newEventDate.substr(
+        5,
+        2
+      )}.${newEventDate.substr(0, 4)}`,
+      guests: [],
+      tasks: [],
+    };
+
+    const newEventId = Date.now().toString();
+
+    await setDoc(
+      doc(getFirestore(), "users", user, "events", newEventId),
+      dataToSet
+    );
+
+    dispatch(
+      addEvent({
+        ...dataToSet,
+        id: newEventId,
+      })
+    );
+
+    setNewEventName("");
+    setNewEventPlace("");
+    setNewEventDate("");
+    setIsShow(false);
+  };
+
+  const onBlurDateInput = (event) => {
+    if (!newEventDate) {
+      event.target.type = "text";
+    }
+  }
 
   return (
     <div className="add-event">
@@ -57,7 +107,7 @@ export const AddEvent = ({ setIsShow }) => {
             className="add-event__input"
             value={newEventDate}
             onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => (e.target.type = "text")}
+            onBlur={(e) => onBlurDateInput(e)}
             onChange={(e) => setNewEventDate(e.target.value)}
           />
           <label
@@ -72,7 +122,7 @@ export const AddEvent = ({ setIsShow }) => {
           </label>
         </div>
         <Button
-          onClickFunction={() => setIsShow(false)}
+          onClickFunction={addEventBtnHandler}
           icon="far fa-plus-square"
           text="Zapisz"
           isStatic={true}
