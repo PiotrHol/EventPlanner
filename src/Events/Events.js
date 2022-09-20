e0e6d0f396eefe7df5631da5a1c3134120cf1060
Piotr Hołub <piotrekholub@wp.pl>
2022-09-20 23:42:58 +0200

    Removed unnecessary old code in Events component

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,66 +1,15 @@
 import React, { useState } from "react";
 import "./events.scss";
-import { setDoc, doc, getFirestore } from "firebase/firestore";
-import { eventValidation } from "../validation";
 import { EventInfo } from "../EventInfo/EventInfo";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Event } from "../Event/Event";
 import { Button } from "../Button/Button";
 import { Popup } from "../Popup/Popup";
 import { AddEvent } from "../AddEvent/AddEvent";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { addEvent } from "../redux/actions/eventsActions";
 
 export const Events = ({ events, isArchive }) => {
-  const [isAddEvent, setIsAddEvent] = useState(false);
-  const [eventName, setEventName] = useState("");
-  const [eventPlace, setEventPlace] = useState("");
-  const [eventDate, setEventDate] = useState("");
-  const [isValidate, setIsValidate] = useState(true);
   const [isShowAddEventPopup, setIsShowAddEventPopup] = useState(false);
   let { path, url } = useRouteMatch();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-
-  const addEventBtnHandler = async (event) => {
-    event.preventDefault();
-    if (!eventValidation(eventName, eventPlace, eventDate)) {
-      setIsValidate(false);
-      return;
-    }
-
-    const dataToSet = {
-      name: eventName,
-      place: eventPlace,
-      date: `${eventDate.substr(8, 2)}.${eventDate.substr(
-        5,
-        2
-      )}.${eventDate.substr(0, 4)}`,
-      guests: [],
-      tasks: [],
-    };
-
-    const newEventId = Date.now().toString();
-
-    await setDoc(
-      doc(getFirestore(), "users", user, "events", newEventId),
-      dataToSet
-    );
-
-    dispatch(
-      addEvent({
-        ...dataToSet,
-        id: newEventId,
-      })
-    );
-
-    setEventName("");
-    setEventPlace("");
-    setEventDate("");
-    setIsValidate(true);
-    setIsAddEvent(false);
-  };
 
   if (!isArchive) {
     return (
@@ -70,11 +19,7 @@ export const Events = ({ events, isArchive }) => {
             <div className="home-page__content-header">
               <h2 className="home-page__content-title">Moje wydarzenia:</h2>
               <Button
-                onClickFunction={() => {
-                  setIsAddEvent((prev) => !prev);
-                  setIsValidate(true);
-                  setIsShowAddEventPopup(true);
-                }}
+                onClickFunction={() => setIsShowAddEventPopup(true)}
                 icon="far fa-plus-square"
                 text="Dodaj wydarzenie"
                 isStatic={false}
@@ -83,42 +28,6 @@ export const Events = ({ events, isArchive }) => {
             <Popup isShow={isShowAddEventPopup} setIsShow={(value) => setIsShowAddEventPopup(value)}>
               <AddEvent setIsShow={(value) => setIsShowAddEventPopup(value)} />
             </Popup>
-            {/* <div className="home-page__add-event">
-              {isAddEvent && (
-                <div className="home-page__add-event-box">
-                  <form>
-                    <div className="home-page__add-event-input">
-                      <h3>Nazwa</h3>
-                      <input
-                        type="text"
-                        maxLength={50}
-                        value={eventName}
-                        onChange={(e) => setEventName(e.target.value)}
-                      />
-                    </div>
-                    <div className="home-page__add-event-input">
-                      <h3>Miejsce</h3>
-                      <input
-                        type="text"
-                        maxLength={50}
-                        value={eventPlace}
-                        onChange={(e) => setEventPlace(e.target.value)}
-                      />
-                    </div>
-                    <div className="home-page__add-event-input">
-                      <h3>Data</h3>
-                      <input
-                        type="date"
-                        value={eventDate}
-                        onChange={(e) => setEventDate(e.target.value)}
-                      />
-                    </div>
-                    {!isValidate && <p>Pola nie mogą być puste!</p>}
-                    <button onClick={addEventBtnHandler}>Dodaj</button>
-                  </form>
-                </div>
-              )}
-            </div> */}
             <div className="home-page__events">
               {events.map((singleEvent) => (
                 <EventInfo
