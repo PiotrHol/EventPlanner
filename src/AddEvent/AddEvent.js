07bf2d9818de89733bc1fa167eec4af4bb2d9e61
Piotr Hołub <piotrekholub@wp.pl>
2022-09-22 22:21:13 +0200

    Added validation to AddEvent component

--- src/AddEvent/AddEvent.js
+++ src/AddEvent/AddEvent.js
@@ -5,6 +5,8 @@ import { setDoc, doc, getFirestore } from "firebase/firestore";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { addEvent } from "../redux/actions/eventsActions";
+import { useForm } from "react-hook-form";
+import classNames from "classnames";
 
 export const AddEvent = ({ setIsShow }) => {
   const [newEventName, setNewEventName] = useState("");
@@ -12,14 +14,13 @@ export const AddEvent = ({ setIsShow }) => {
   const [newEventDate, setNewEventDate] = useState("");
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const addEventBtnHandler = async (event) => {
-    event.preventDefault();
-    // if (!eventValidation(eventName, eventPlace, eventDate)) {
-    //   setIsValidate(false);
-    //   return;
-    // }
-
+  const addEventBtnHandler = async (data) => {
     const dataToSet = {
       name: newEventName,
       place: newEventPlace,
@@ -51,31 +52,29 @@ export const AddEvent = ({ setIsShow }) => {
     setIsShow(false);
   };
 
-  const onBlurDateInput = (event) => {
-    if (!newEventDate) {
-      event.target.type = "text";
-    }
-  }
-
   return (
     <div className="add-event">
       <h2 className="add-event__title">Dodaj wydarzenie</h2>
-      <form>
+      <form onSubmit={handleSubmit(addEventBtnHandler)}>
         <div className="add-event__input-box">
           <input
             type="text"
-            className="add-event__input"
+            {...register("eventName", {
+              required: true,
+              maxLength: 50,
+            })}
+            className={classNames("add-event__input", {
+              "add-event__input--error": errors.eventName,
+            })}
             maxLength={50}
             id="new-event-name"
             value={newEventName}
             onChange={(e) => setNewEventName(e.target.value)}
           />
           <label
-            className={
-              newEventName
-                ? "add-event__label add-event__label--focus"
-                : "add-event__label"
-            }
+            className={classNames("add-event__label", {
+              "add-event__label--focus": newEventName,
+            })}
             htmlFor="new-event-name"
           >
             Nazwa
@@ -84,18 +83,22 @@ export const AddEvent = ({ setIsShow }) => {
         <div className="add-event__input-box">
           <input
             type="text"
-            className="add-event__input"
+            {...register("eventPlace", {
+              required: true,
+              maxLength: 50,
+            })}
+            className={classNames("add-event__input", {
+              "add-event__input--error": errors.eventPlace,
+            })}
             maxLength={50}
             id="new-event-place"
             value={newEventPlace}
             onChange={(e) => setNewEventPlace(e.target.value)}
           />
           <label
-            className={
-              newEventPlace
-                ? "add-event__label add-event__label--focus"
-                : "add-event__label"
-            }
+            className={classNames("add-event__label", {
+              "add-event__label--focus": newEventPlace,
+            })}
             htmlFor="new-event-place"
           >
             Miejsce
@@ -103,30 +106,30 @@ export const AddEvent = ({ setIsShow }) => {
         </div>
         <div className="add-event__input-box">
           <input
-            type="text"
-            className="add-event__input"
+            type="date"
+            {...register("eventDate", { required: true })}
+            className={classNames(
+              "add-event__input",
+              "add-event__input--date",
+              { "add-event__input--focus": newEventDate },
+              { "add-event__input--error": errors.eventDate }
+            )}
             value={newEventDate}
-            onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => onBlurDateInput(e)}
-            onChange={(e) => setNewEventDate(e.target.value)}
+            id="new-event-date"
+            onChange={(e) => {
+              setNewEventDate(e.target.value);
+            }}
           />
           <label
-            className={
-              newEventDate
-                ? "add-event__label add-event__label--focus"
-                : "add-event__label"
-            }
+            className={classNames("add-event__label", {
+              "add-event__label--focus": newEventDate,
+            })}
             htmlFor="new-event-date"
           >
             Data
           </label>
         </div>
-        <Button
-          onClickFunction={addEventBtnHandler}
-          icon="far fa-plus-square"
-          text="Zapisz"
-          isStatic={true}
-        />
+        <Button icon="far fa-plus-square" text="Zapisz" isStatic={true} />
       </form>
     </div>
   );
