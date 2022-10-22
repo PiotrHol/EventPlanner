81126c2e5f3187c117b176513edee5b840bb54ed
Piotr Hołub <piotrekholub@wp.pl>
2022-10-22 13:15:45 +0200

    Updated structure of Event component

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -8,13 +8,15 @@ import { Cost } from "../Cost/Cost";
 export const Event = ({ id, name, place, date, tasks, guests }) => {
   return (
     <div className="event-page">
-      <div className="event-page__left-content">
-        <Information eventId={id} name={name} place={place} date={date} />
-        <Tasks eventId={id} tasks={tasks} />
-      </div>
-      <div className="event-page__right-content">
-        <Guests eventId={id} guests={guests} />
-        <Cost tasks={tasks} />
+      <div className="event-page__main-content">
+        <div className="event-page__left-content">
+          <Information eventId={id} name={name} place={place} date={date} />
+          <Tasks eventId={id} tasks={tasks} />
+        </div>
+        <div className="event-page__right-content">
+          <Guests eventId={id} guests={guests} />
+          <Cost tasks={tasks} />
+        </div>
       </div>
     </div>
   );
