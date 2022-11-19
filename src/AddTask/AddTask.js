d0e05204a7e9770e17af99e2dd91f913b0660ab3
Piotr Hołub <piotrekholub@wp.pl>
2022-11-19 18:17:17 +0100

    Added form's validation in AddTask component

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import "./addTask.scss";
 import { Button } from "../Button/Button";
 import classNames from "classnames";
+import { useForm } from "react-hook-form";
 
 export const AddTask = ({ setIsShow }) => {
   const [newTaskName, setNewTaskName] = useState("");
   const [newTaskDescription, setNewTaskDescription] = useState("");
   const [newTaskCost, setNewTaskCost] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const addTaskBtnHandler = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="add-task">
       <h2 className="add-task__title">Dodaj zadanie</h2>
-      <form>
+      <form onSubmit={handleSubmit(addTaskBtnHandler)}>
         <div className="add-task__input-box">
           <input
             type="text"
-            className="add-task__input"
+            {...register("taskName", {
+              required: true,
+              maxLength: 50,
+            })}
+            className={classNames("add-task__input", {
+              "add-task__input--error": errors.taskName,
+            })}
+            maxLength={60}
             id="new-task-name"
             value={newTaskName}
             onChange={(e) => setNewTaskName(e.target.value)}
@@ -32,24 +49,46 @@ export const AddTask = ({ setIsShow }) => {
         <div className="add-task__input-box">
           <input
             type="text"
-            className="add-task__input"
+            {...register("taskDescription", {
+              required: true,
+              maxLength: 50,
+            })}
+            className={classNames("add-task__input", {
+              "add-task__input--error": errors.taskDescription,
+            })}
+            maxLength={60}
             id="new-task-description"
             value={newTaskDescription}
             onChange={(e) => setNewTaskDescription(e.target.value)}
           />
-          <label className="add-task__label" htmlFor="new-task-description">
+          <label
+            className={classNames("add-task__label", {
+              "add-task__label--focus": newTaskDescription,
+            })}
+            htmlFor="new-task-description"
+          >
             Opis
           </label>
         </div>
         <div className="add-task__input-box">
           <input
             type="number"
-            className="add-task__input"
+            {...register("taskCost", {
+              max: 100000000,
+            })}
+            className={classNames("add-task__input", {
+              "add-task__input--error": errors.taskCost,
+            })}
             id="new-task-cost"
             value={newTaskCost}
             onChange={(e) => setNewTaskCost(e.target.value)}
           />
-          <label className="add-task__label" htmlFor="new-task-cost">
+          <label
+            className={classNames("add-task__label", {
+              "add-task__label--focus": newTaskCost,
+            })}
+            htmlFor="new-task-cost"
+          >
             Kwota{" "}
             {!newTaskCost && (
               <span className="add-task__label--additional">(Opcjonalnie)</span>
