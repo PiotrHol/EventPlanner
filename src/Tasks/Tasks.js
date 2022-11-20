47db9f173c4791abd882ee19ce768f3b13af3956
Piotr Hołub <piotrekholub@wp.pl>
2022-11-20 23:30:57 +0100

    Deleted unnecessary functions in Tasks component

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -1,57 +1,12 @@
 import React, { useState } from "react";
 import "./tasks.scss";
-import { eventValidation } from "../validation";
-import { doc, updateDoc, arrayUnion, getFirestore } from "firebase/firestore";
 import { Task } from "../Task/Task";
 import { Button } from "../Button/Button";
 import { Popup } from "../Popup/Popup";
 import { AddTask } from "../AddTask/AddTask";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { addNewTask } from "../redux/actions/eventsActions";
 
 export const Tasks = ({ eventId, tasks }) => {
-  const [isAddTask, setIsAddTask] = useState(false);
-  const [taskName, setTaskName] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  const [taskCost, setTaskCost] = useState("");
-  const [isValid, setIsValid] = useState(true);
   const [isShowAddTaskPopup, setIsShowAddTaskPopup] = useState(false);
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  const addTaskBtnHandler = () => {
-    setIsAddTask((prev) => !prev);
-    setTaskName("");
-    setTaskDescription("");
-    setTaskCost("");
-    !isValid && setIsValid(true);
-  };
-
-  const saveTaskBtnHandler = async (event) => {
-    event.preventDefault();
-    if (!eventValidation(taskName, taskDescription)) {
-      setIsValid(false);
-      return;
-    }
-
-    const dataToSet = {
-      id: Date.now(),
-      name: taskName,
-      description: taskDescription,
-      cost: !taskCost ? 0 : parseFloat(parseFloat(taskCost).toFixed(2)),
-      isDone: false,
-    };
-
-    await updateDoc(doc(getFirestore(), "users", user, "events", eventId), {
-      tasks: arrayUnion(dataToSet),
-    });
-
-    dispatch(addNewTask(eventId, dataToSet));
-
-    setIsAddTask(false);
-    setIsValid(true);
-  };
 
   return (
     <div className="event-page__content event-page__tasks">
