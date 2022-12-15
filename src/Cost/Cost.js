8fb709688c40e69e86aed0100e426c2fc82d46b6
Piotr Hołub <piotrekholub@wp.pl>
2022-12-15 22:34:14 +0100

    Changed structure of Cost component

--- src/Cost/Cost.js
+++ src/Cost/Cost.js
@@ -1,23 +1,11 @@
 import React from "react";
 import "./cost.scss";
 
-export const Cost = ({tasks}) => {
-
-    return (
-        <div className="event-page__content event-page__cost">
-            <div className="event-page__bar"/>
-            <div className="event-page__cost-content">
-                <div className="event-page__cost-main">
-                    <h3>Koszty:</h3>
-                    <h3 className="event-page__cost-main-value">{tasks.reduce((allCost, singleCost) => allCost + singleCost.cost, 0)} zł</h3>
-                </div>
-                <h3 className="event-page__cost-single-title">Szczegółowe koszty:</h3>
-                <ul className="event-page__cost-single">
-                    {tasks.map(({id, name, cost}) => cost !== 0 && <li key={id}>
-                    <span className="fas fa-caret-right"/>{name} - <span className="event-page__cost-single-value">{cost} zł</span>
-                    </li>)}
-                </ul>
-            </div>
-        </div>
-    );
-}
+export const Cost = ({ name, cost }) => {
+  return (
+    <div className="event-page__cost">
+      <h3 className="event-page__cost-title">{name}</h3>
+      <h3 className="event-page__cost-value">{cost} zł</h3>
+    </div>
+  );
+};
